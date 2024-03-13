856b92802da6f36f3ca61752206353237289d979
ghaydn <Ghaydn@users.noreply.github.com>
2024-03-13 23:38:06 +0300

    Added LCD support

--- blinker_mega.lua
+++ blinker_mega.lua
@@ -30,11 +30,15 @@ local sequence = { 		-- blinking sequence
 --	{"red", "blue", {channel = "foo", msg = "bar"}},    -- any reasonable number of steps is allowed
 }
 
+-- channel name to report state
+local lcd = "lcd"
+
 --- These variables can be undefined
 local switch = "red"      		-- if undefined, then autostart will be enabled anyway
 local autostart = false			-- if true, will autostart even if switch is defined
 
-local detector-- = "black"      -- if defined, then after wait time blinker will automatically
+local detector-- = "black"      -- if defined, then after wait time blinker will automatically stop blinking
+								-- conduit can also be a detector
 -- detector = "self"        	-- "self" means that detector is this tube detecting an item
 local wait_time = 5				-- turn off if there will be no new signals on detector port
 
@@ -56,8 +60,9 @@ local digiline_channel = "blinker"
 --
 -- {command = "configure",     -- changes parameters, such as:
 --    sequence = {},              --
---    rate = 0.2                  --
---    sorting = {}                --
+--    rate = 0.2,                 --
+--    sorting = {},               --
+--    wait_time = 5,              --
 -- }
 
 -- if any input port is defined, then this port will be skipped while playing the sequence
@@ -76,8 +81,11 @@ local sorting = {
 -----------------------------------------------------
 -----------------------------------------------------
 -- FUNCTIONS-----------------------------------------
-	
-local blink = function()
+
+local function blink()
+  
+  digiline_send(lcd, "Timer: " .. tostring(wait_time - mem.var.time) .. ", step: "..tostring(mem.var.step))
+  
   for p, v in pairs(port) do
 	port[p] = false
   end
@@ -96,52 +104,39 @@ local blink = function()
 
   mem.var.step = mem.var.step + 1
   if mem.var.step > #mem.var.sequence then mem.var.step = 1 end
+  
+  --if mem.var.step == 1 and detector ~= nil then
+	digiline_send(detector, "inventory")
+  --end
 end
 
 -----------------------------------------------------
 
-local start_blink = function()
+local function start_blink ()
   mem.var.time = 0
   mem.var.step = 1
   mem.var.blink = true
   interrupt(mem.var.rate, "blink")
   blink()
+  digiline_send(lcd, "Starting blinks")
 end
 
+-----------------------------------------------------
+
 -- set all ports off
-local end_blink = function()
+local function end_blink()
   mem.var.blink = false
   for p, _ in pairs(port) do
 	port[p] = false
   end
-  
+  digiline_send(lcd, "Stopped")
 end
-
+	
 -----------------------------------------------------
------------------------------------------------------
--- EVENTS--------------------------------------------
 
-if event.type == "program" then
-  mem.var = {
-	blink = false,
-	step = 1,  
-	time = 0,
-	sequence = sequence,
-	rate = rate,
-	sorting = sorting,
-  }
+local function after_blink()
 
-  if not switch or autostart then
-	start_blink()
-  end
-end
-
-
----Main timer
-if event.type == "interrupt" and event.iid == "blink" then
-  if mem.var.blink then
-	blink()  
-	if wait_time and wait_time > 0 and detector then
+	if mem.var.wait_time and mem.var.wait_time > 0 and detector then
 		
 	  -- add time only if no signal on detector
 	  if port[detector] then
@@ -150,14 +145,127 @@ if event.type == "interrupt" and event.iid == "blink" then
 		mem.var.time = mem.var.time + mem.var.rate
 	  end
 	  
-	  if mem.var.time > wait_time then
-        end_blink()
+	  if mem.var.time > mem.var.wait_time then
+		end_blink()
 	  else
 		interrupt(mem.var.rate, "blink")  
 	  end
 	else  
-      interrupt(mem.var.rate, "blink")
+	  interrupt(mem.var.rate, "blink")
 	end
+end
+
+-----------------------------------------------------
+
+local function ready()
+  mem.var = {
+	blink = false,
+	step = 1,  
+	time = 0,
+	wait_time = wait_time,
+	sequence = sequence,
+	rate = rate,
+	sorting = sorting,
+	count = 0,
+  }
+
+  if not switch or autostart then
+	start_blink()
+  end
+  
+  digiline_send(lcd, "Starting")
+end
+
+-----------------------------------------------------
+
+local function msg_command(msg)
+
+	if msg.command == "start" or msg.command == "on" then
+		start_blink()
+		
+	elseif msg.command == "stop" or msg.command == "off" then
+		end_blink()
+		
+	elseif msg.command == "detected" then
+		mem.var.time = 0
+		
+	elseif msg.command == "configure" then
+
+		if msg.sequence ~= nil and type(msg.sequence) == "table" then
+			mem.var.sequence = msg.sequence
+		end
+		
+		if msg.rate ~= nil and type(msg.rate) == "number" and msg.rate > 0 then
+			mem.var.rate = msg.rate
+		end
+		
+		if msg.sorting ~= nil and type(msg.sorting) == "table" then
+			mem.var.sorting = msg.sorting
+		end
+		
+		if msg.wait_time ~= nil and type(msg.wait_time) == "number" then
+			mem.var.wait_time = msg.wait_time
+		end
+		
+	end
+end
+
+-----------------------------------------------------
+
+local function digiline(channel, msg)
+
+	if channel == switch then
+		if msg == "on" then
+			start_blink()
+		elseif msg == "off" then
+			end_blink()
+		end
+		
+	elseif channel == detector then
+		if msg.inventory ~= nil then
+			mem.var.count = 0
+			for _, v in ipairs(msg.inventory) do
+				mem.var.count = mem.var.count + v.count
+			end
+			if mem.var.count > 0 then
+				mem.var.time = 0
+			end
+		end
+		
+	elseif channel == killswitch then
+		end_blink()
+		
+	elseif channel == digiline_channel then
+		if type(msg) == "string" then
+		
+			if msg == "on" or msg == "start" then
+				start_blink()
+				
+			elseif msg == "off" or msg == "stop" then
+				end_blink()
+			end
+			
+		elseif type(msg) == "table" then
+			msg_command(msg)
+		
+		end
+	end
+end
+
+-----------------------------------------------------
+-----------------------------------------------------
+-- EVENTS--------------------------------------------
+
+if event.type == "program" then
+  ready()
+end
+
+
+---Main timer
+if event.type == "interrupt" and event.iid == "blink" then
+  if mem.var.blink then
+	blink()
+	after_blink()
   end
 end
 
@@ -218,54 +326,6 @@ end
 ----------
 
 if event.type == "digiline" then
+	digiline(event.channel, event.msg)
 	
-	if event.channel == switch then
-		if event.msg == "on" then
-			start_blink()
-		elseif event.msg == "off" then
-			end_blink()
-		end
-	elseif event.channel == detector then
-		mem.var.time = 0
-	elseif event.channel == killswitch then
-		end_blink()
-	elseif channel == digiline_channel then
-		local msg = event.msg
-		if type(msg) == "string" then
-		
-			if msg == "on" or msg == "start" then
-				start_blink()
-				
-			elseif msg == "off" or msg == "stop" then
-				end_blink()
-			end
-			
-		elseif type(msg) == "table" then
-		
-			if msg.command == "start" or msg.command == "on" then
-				start_blink()
-				
-			elseif msg.command == "stop" or msg.command == "off" then
-				end_blink()
-				
-			elseif msg.command == "detected" then
-				mem.var.time = 0
-				
-			elseif msg.command == "configure" then
-			
-				if msg.sequence ~= nil and type(msg.sequence) == "table" then
-					mem.var.sequence = msg.sequence
-				end
-				
-				if msg.rate ~= nil and type(msg.rate) == "number" and msg.rate > 0 then
-					mem.var.rate = msg.rate
-				end
-				
-				if msg.sorting ~= nil and type(msg.sorting) == "table" then
-					mem.var.sorting = msg.sorting
-				end
-				
-			end
-		end
-	end
 end
